872af6acfe2c914ec53fd0e30ae083af6a9a3edb
Thiago kauai <html5ephp@gmail.com>
2019-08-07 20:46:24 -0300

    filtrando usuarios,like,deslike logando usuario

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,12 @@
 import React,{ useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import api from '../services/api'
 import logo from '../assets/logo.svg'
 import like from '../assets/like.svg'
 import deslike from '../assets/dislike.svg'
 import '../pages/Main.css'
 
-export default function Main({ match:{ params }}) {
+export default function Main({ match: { params } } ) {
     const [ users, setUsers ] = useState([])
 
     useEffect(() => {
@@ -14,9 +15,22 @@ export default function Main({ match:{ params }}) {
        .then(item => setUsers(item.data))
     },[params.id])
 
-    return (
+    const handleLike = async (id) => {
+        const header = { headers: { user:params.id } }
+        await api.post(`/devs/${id}/likes`,null,header)
+    }
+
+    const handleDeslike = async (id) => {
+        const header = { headers: { user:params.id } }
+        await api.post(`/devs/${id}/deslikes`,null,header)
+        setUsers(users.filter(item => item._id != id))
+    }
+
+    return users.length ? (
        <div className="main-container">
-           <img src={logo} alt=""/>
+           <Link to="/">
+             <img src={logo} alt=""/>
+           </Link>
            <ul>
 {
     users.map(item => {
@@ -27,13 +41,20 @@ export default function Main({ match:{ params }}) {
                     <p>{item.bio}</p>
                 </footer>
                 <div className="buttons">
-                    <button type="button"><img src={like} alt=""/></button>
-                    <button src={deslike} type="button"><img src={deslike} alt=""/></button>
+                    <button onClick={() => handleLike(item._id)} type="button"><img src={like} alt=""/></button>
+                    <button onClick={() => handleDeslike(item._id)} src={deslike} type="button"><img src={deslike} alt=""/></button>
                 </div>
             </li>
     })
 }
-           </ul>
-       </div>   
+        </ul>
+       </div> 
+    ) : (
+        <div className="main-container">
+             <Link to="/">
+               <img src={logo} alt=""/>
+             </Link> 
+            <div><h2 class="no-followers">Nao temos usuarios cadastrados que <span style={{ fontSize:30 }}>💩💩💩</span></h2></div>
+        </div>
     )
 }
