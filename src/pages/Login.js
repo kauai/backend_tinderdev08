1703b9fb43bcc571fca2e66c0b28ec3de201f748
Thiago kauai <html5ephp@gmail.com>
2019-08-07 13:44:46 -0300

    controlled components

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import './Loggin.css'
 import logo from '../assets/logo.svg'
 
-const Login = () => (
+const Login = () => {
+ const [ userName, setUserName ] = useState({ name: '' ,password:''})
+
+ function user(e){
+    setUserName({...userName,[e.target.name]:e.target.value })
+ }
+
+ return (
     <div className="login-container">
         <form>
+          {JSON.stringify(userName)}
           <img src={logo} alt="tinDev"/>
-          <input type="text" placeholder="Digite seu usuario no github"/>
+          <input 
+          name="name"
+          user={userName.name}
+          type="text" 
+          placeholder="Digite seu usuario no github"
+          onChange={user}
+          />
+          <input 
+          name="password"
+          user={userName.password}
+          type="text" 
+          placeholder="Digite sua senha"
+          onChange={user}
+          />
           <button type="submit">Enviar</button>
         </form> 
     </div>
 )
+}
 
 export default Login
